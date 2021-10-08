28b744da3c85ec314c6e1d0f71153575b60ef7d6
rmasutani <masutanirugby@gmail.com>
2021-10-08 08:24:41 +0000

    Fix test.sh

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -9,9 +9,9 @@ assert() {
     actual="$?"
 
     if [ "$actual" = "$expected" ]; then
-        echo "$input" => "$actual"
+        echo "$input => $actual"
     else
-        echo "$input" => "$expected" expected, but got "$actual"
+        echo "$input => $expected expected, but got $actual"
         exit 1
     fi
 }
@@ -19,5 +19,6 @@ assert() {
 assert 0 0
 assert 42 42
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
