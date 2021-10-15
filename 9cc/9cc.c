0bcbd1e4b18d212db23e2f36f326d33fb1185282
rmasutani <masutanirugby@gmail.com>
2021-10-15 05:10:16 +0000

    Add node parsing functions

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -123,6 +123,84 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+typedef enum
+{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val; // kindがNUMのときだけ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr()
+{
+    Node *node = mul();
+
+    for (;;)
+    {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul()
+{
+    Node *node = primary();
+
+    for (;;)
+    {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary()
+{
+    if (consume('('))
+    {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -133,16 +211,6 @@ int main(int argc, char **argv)
 
     token = tokenize(argv[1]);
     user_input = argv[1];
-
-    printf("Kind: %d\n", token->kind);
-    printf("str: %s\n", token->str);
-    printf("val: %d\n", token->val);
-    printf("\n");
-    printf("next kind: %d\n", token->next->kind);
-    printf("next str: %s\n", token->next->str);
-    printf("next val: %d\n", token->next->val);
-
-    printf("next next val: %d\n", token->next->next->val);
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
